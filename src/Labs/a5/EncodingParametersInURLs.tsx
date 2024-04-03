0d13cf47fc371fe1dfe0f28e96b4dcdf1e7a8953
lilychan16 <chen460@indiana.edu>
2024-04-03 08:36:29 -0400

    complete lab

---
+++ src/Labs/a5/EncodingParametersInURLs.tsx
@@ -0,0 +1,120 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+function EncodingParametersInURLs() {
+  const [a, setA] = useState(34);
+  const [b, setB] = useState(23);
+  const [welcome, setWelcome] = useState("");
+  const [result, setResult] = useState(0);
+  
+  const fetchWelcome = async () => {
+    const response = await axios.get("http://localhost:4000/a5/welcome");
+    setWelcome(response.data);
+  };
+  useEffect(() => {
+    fetchWelcome();
+  }, []);
+
+  const fetchSum = async (a: number, b: number) => {
+    const response = await axios.get(`http://localhost:4000/a5/add/${a}/${b}`);
+    setResult(response.data);
+  };
+  const fetchSubtraction = async (a: number, b: number) => {
+    const response = await axios.get(
+      `http://localhost:4000/a5/subtract/${a}/${b}`
+    );
+    setResult(response.data);
+  };
+
+  return (
+    <div>
+      <h3>Encoding Parameters In URLs</h3>
+      <h4>Integrating React with APIs</h4>
+      <h5>Fetching Welcome</h5>
+      <h6>{welcome}</h6>
+      <br />
+      <h4>Calculator</h4>
+      <input
+        type="number"
+        value={a}
+        onChange={(e) => setA(parseInt(e.target.value))}
+      />
+      <br />
+      <input
+        type="number"
+        value={b}
+        onChange={(e) => setB(parseInt(e.target.value))}
+      />
+      <br />
+      <input value={result} type="number" readOnly />
+      <h3>Fetch Result</h3>
+      <button 
+        className="btn btn-primary mb-2" 
+        onClick={() => fetchSum(a, b)}
+      >
+        Fetch Sum of {a} + {b}
+      </button>
+      <br />
+      <button
+        className="btn btn-danger mb-2"
+        onClick={() => fetchSubtraction(a, b)}
+      >
+        Fetch Substraction of {a} - {b}
+      </button>
+      <h3>Path Parameters</h3>
+      <a
+        className="btn btn-primary me-2"
+        href={`http://localhost:4000/a5/add/${a}/${b}`}
+      >
+        Add {a} + {b}
+      </a>
+      <a
+        className="btn btn-danger me-2"
+        href={`http://localhost:4000/a5/subtract/${a}/${b}`}
+      >
+        Substract {a} - {b}
+      </a>
+      <a
+        className="btn btn-warning me-2"
+        href={`http://localhost:4000/a5/multiply/${a}/${b}`}
+      >
+        Multiply {a} * {b}
+      </a>
+      <a
+        className="btn btn-success"
+        href={`http://localhost:4000/a5/divide/${a}/${b}`}
+      >
+        Divide {a} / {b}
+      </a>
+      <br />
+      <h3>Query Parameters</h3>
+      <a
+        className="btn btn-primary me-2"
+        href={`http://localhost:4000/a5/calculator?operation=add&a=${a}&b=${b}`}
+      >
+        Add {a} + {b}
+      </a>
+      <a
+        className="btn btn-danger me-2"
+        href={`http://localhost:4000/a5/calculator?operation=subtract&a=${a}&b=${b}`}
+      >
+        Substract {a} - {b}
+      </a>
+      <a
+        className="btn btn-warning me-2"
+        href={`http://localhost:4000/a5/calculator?operation=multiply&a=${a}&b=${b}`}
+      >
+        Multiply {a} * {b}
+      </a>
+      <a
+        className="btn btn-success"
+        href={`http://localhost:4000/a5/calculator?operation=divide&a=${a}&b=${b}`}
+      >
+        Divide {a} / {b}
+      </a>
+      <br />
+      <br />
+    </div>
+  );
+}
+export default EncodingParametersInURLs;
---
+++ src/Labs/a5/WorkingWithObjects.tsx
@@ -0,0 +1,166 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+function WorkingWithObjects() {
+    const [assignment, setAssignment] = useState({
+      id: 1,
+      title: "NodeJS Assignment",
+      description: "Create a NodeJS server with ExpressJS",
+      due: "2021-10-10",
+      completed: false,
+      score: 0
+    });
+    const ASSIGNMENT_URL = "http://localhost:4000/a5/assignment";
+
+    const [module, setModule] = useState({
+      id: "M1",
+      name: "NodeJS",
+      description: "NodeJS basics",
+      course: "RS101"
+    });
+    const MODULE_URL = "http://localhost:4000/a5/module";
+
+    const fetchAssignment = async () => {
+      const response = await axios.get(`${ASSIGNMENT_URL}`);
+      setAssignment(response.data);
+    };
+    const updateTitle = async () => {
+      const response = await axios.get(
+        `${ASSIGNMENT_URL}/title/${assignment.title}`
+      );
+      setAssignment(response.data);
+    };
+    useEffect(() => {
+      fetchAssignment();
+    }, []);
+
+    return (
+      <div>
+        <h3>Working With Objects</h3>
+        <h4>Retrieving Objects</h4>
+        <a
+          className="btn btn-primary"
+          href="http://localhost:4000/a5/assignment"
+        >
+          Get Assignment
+        </a>
+        <h4>Retrieving Properties</h4>
+        <a
+          className="btn btn-primary"
+          href="http://localhost:4000/a5/assignment/title"
+        >
+          Get Title
+        </a>
+        <a
+          className="btn btn-primary ms-2"
+          href="http://localhost:4000/a5/assignment/score"
+        >
+          Get Score
+        </a>
+        <a
+          className="btn btn-primary ms-2"
+          href="http://localhost:4000/a5/assignment/completed"
+        >
+          Get Completed Status
+        </a>
+        <h4>Modifying Properties</h4>
+        <input
+          type="text"
+          onChange={(e) =>
+            setAssignment({ ...assignment, title: e.target.value })
+          }
+          value={assignment.title}
+        />
+        <button className="btn btn-primary ms-2 mb-2" onClick={updateTitle}>
+          Update Title to: {assignment.title}
+        </button>
+        <a
+          className="btn btn-primary ms-2 mb-2"
+          href={`${ASSIGNMENT_URL}/title/${assignment.title}`}
+        >
+          Update Title
+        </a>
+        <br />
+        <button className="btn btn-danger mb-2" onClick={fetchAssignment}>
+          Fetch Assignment
+        </button>
+        <br />
+        <input
+          type="number"
+          onChange={(e) =>
+            setAssignment({ ...assignment, score: parseInt(e.target.value) })
+          }
+          value={assignment.score}
+        />
+        <a
+          className="btn btn-primary ms-2 mb-2"
+          href={`${ASSIGNMENT_URL}/score/${assignment.score}`}
+        >
+          Update Score
+        </a>
+        <br />
+        <input
+          type="checkbox"
+          id="completed"
+          onChange={(e) =>
+            setAssignment({ ...assignment, completed: e.target.checked })
+          }
+          checked={assignment.completed}
+        />
+        <label htmlFor="completed">Completed</label>
+        <a
+          className="btn btn-primary ms-2"
+          href={`${ASSIGNMENT_URL}/completed/${assignment.completed}`}
+        >
+          Update Completed
+        </a>
+
+        <h4>Retrieving Module</h4>
+        <a className="btn btn-primary" href="http://localhost:4000/a5/module">
+          Get Module
+        </a>
+        <h4>Retrieving Module Name</h4>
+        <a
+          className="btn btn-primary"
+          href="http://localhost:4000/a5/module/name"
+        >
+          Get Module Name
+        </a>
+        <a
+          className="btn btn-primary ms-2"
+          href="http://localhost:4000/a5/module/description"
+        >
+          Get Module Description
+        </a>
+        <h4>Modifying Module Properties</h4>
+        <input
+          type="text"
+          onChange={(e) => setModule({ ...module, name: e.target.value })}
+          value={module.name}
+        />
+        <a
+          className="btn btn-primary ms-2 mb-2"
+          href={`${MODULE_URL}/name/${module.name}`}
+        >
+          Update Module Name
+        </a>
+        <br />
+        <input
+          type="text"
+          onChange={(e) =>
+            setModule({ ...module, description: e.target.value })
+          }
+          value={module.description}
+        />
+        <a
+          className="btn btn-primary ms-2"
+          href={`${MODULE_URL}/description/${module.description}`}
+        >
+          Update Module Description
+        </a>
+        <br />
+        <br />
+      </div>
+    );
+}
+export default WorkingWithObjects;
