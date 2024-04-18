70ab35696c3d8ba45aa2c99c99d5c5da77c303a5
maggieliu <magliudh@gmail.com>
2024-04-18 17:21:38 -0400

    finished basic looks of editor side tab

--- src/Kanbas/Courses/Quizzes/Editor.tsx
+++ src/Kanbas/Courses/Quizzes/Editor.tsx
@@ -1,35 +1,54 @@
 import React from "react";
-import { NavLink, Routes, Route, useLocation } from "react-router-dom";
+import { NavLink, Routes, Route, Navigate } from "react-router-dom";
 import MultipleChoice from "./Editors/MultipleChoice";
 import QuizEditor from "./QuizEditor";
+import { useSelector } from "react-redux";
+import { KanbasState } from "../../store";
+import { FaCheckCircle, FaBan } from "react-icons/fa";
 
 function Editor() {
-  const location = useLocation();
-
-  // Function to determine class name based on current location
-  const getNavLinkClass = (path: any) => {
-    return `nav-link ${location.pathname.includes(path) ? "active" : ""}`;
-  };
-
+  const quizData = useSelector(
+    (state: KanbasState) => state.quizzesReducer.quiz
+  );
+  console.log(quizData);
   return (
     <div>
-      <h1>Edit Quiz</h1>
+      <div className="d-flex justify-content-end align-items-center mb-3">
+        <h3 className="me-3">Points: {quizData.points}</h3>
+        <h3 style={{ color: quizData.published ? "green" : "grey" }}>
+          {quizData.published ? (
+            <>
+              <FaCheckCircle /> published
+            </>
+          ) : (
+            <>
+              <FaBan /> Not published
+            </>
+          )}
+        </h3>
+      </div>
+      <hr />
       <nav className="nav nav-tabs mt-2">
         <NavLink
           to="EditDetails"
-          className={() => getNavLinkClass("EditDetails")}
+          className={({ isActive }) =>
+            isActive ? "nav-link active" : "nav-link"
+          }
         >
           Details
         </NavLink>
         <NavLink
           to="MultipleChoice"
-          className={() => getNavLinkClass("MultipleChoice")}
+          className={({ isActive }) =>
+            isActive ? "nav-link active" : "nav-link"
+          }
         >
           Questions
         </NavLink>
       </nav>
 
       <Routes>
+        <Route index element={<Navigate replace to="EditDetails" />} />
         <Route path="EditDetails" element={<QuizEditor />} />
         <Route path="MultipleChoice" element={<MultipleChoice />} />
       </Routes>
